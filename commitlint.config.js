ce1ced4cc813ee8c2f3c9b52642049ac09b86fcb
FE92star <1737765939@qq.com>
2022-07-27 13:38:34 +0800

    feat: commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -18,7 +18,7 @@ module.exports = {
         'types',
         'release',
         'deps'
-      ] // [新功能，修复bug，文档，重构，纯样式修改，测试，构建过程或辅助工具的变动]
+      ] // [新功能，修复bug，文档，重构，纯样式修改，测试，构建过程或辅助工具的变动，性能优化，工作区流程变更，构建，类型，发版，依赖]
     ],
     'body-leading-blank': [2, 'always'],
     'footer-leading-blank': [2, 'always']
